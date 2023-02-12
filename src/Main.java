789d5f4049dc51e2f9b094c49ab5925d9ad597e5
Bobrov Igor <bobrov.i.o@live.ru>
2023-02-12 20:11:27 +0300

    Bonus

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676; // Стоимость билета
+        int x = 20; // 1 миля
+
+        int bonus = price / x; // Количество бонусных миль
+        System.out.println(bonus + " бонусная(ых) миля(ь)");
+    }
+}
